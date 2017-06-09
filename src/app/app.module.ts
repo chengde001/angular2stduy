3ba6fd24940b32b6f937511c918ad2ab8d2bcd17
xChengdePiao <xChengde.Piao@jp.sony.com>
2017-06-09 12:58:43 +0900

    4.MULTIPLE COMPONENTS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
 
 @NgModule({
   imports:      [ 
     BrowserModule,
     FormsModule,
   ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeroDetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
